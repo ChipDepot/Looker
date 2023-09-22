657c195f8e43c38de0ca7019411a54b9271d4829
dnl <danieldavid2001@gmail.com>
2023-09-21 22:17:03 -0500

    Quick update on the main

--- looker/src/main.rs
+++ looker/src/main.rs
@@ -4,17 +4,15 @@ mod utils;
 #[macro_use]
 extern crate log;
 
-use dotenv;
 use red::connector::RedisConnector;
 use utils::{env_handler, env_keys};
 
 fn main() {
-    // Set log level to debug so all things show up
+    // Set log level to debug so all things show up and start the logger
     std::env::set_var("RUST_LOG", "debug");
-
-    // Start the logger
     env_logger::init();
 
     // Load the env variables
     env_handler::load_env(None);
+
 }
